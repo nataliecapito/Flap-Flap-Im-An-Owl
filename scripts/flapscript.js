2bf2e8f7387c1c4d799c45d6e8fd1d1e8c95febc
njcapito <njcapito@aol.com>
2018-01-13 10:34:29 -0500

    fixing

--- scripts/flapscript.js
+++ scripts/flapscript.js
@@ -1,1 +1,8 @@
 
+function changeImage() {
+	if (document.getElementById("imageHeadStyle").src == "images/head1.png") {
+		document.getElementById("imageHeadStyle").src = "images/head3.png";
+	} else {
+		document.getElementById("imageHeadStyle").src = "images/head1.png";
+	}
+}
